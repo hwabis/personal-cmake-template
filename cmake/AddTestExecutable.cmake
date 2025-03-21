bc984fe82cfb2d76e1bda555b2e170841d3ec238
hwabis <kennysun23@gmail.com>
2025-03-21 16:12:40 -0400

    streamline unit testing

---
+++ cmake/AddTestExecutable.cmake
@@ -0,0 +1,16 @@
+include(${CMAKE_SOURCE_DIR}/cmake/SetTargetDefaults.cmake)
+
+function(add_test_executable target test_lib)
+  add_executable(${target} ${ARGN})
+
+  set_target_defaults(${target})
+
+  target_include_directories(${target} SYSTEM PRIVATE
+    "${googletest_SOURCE_DIR}/include"
+  )
+  
+  target_link_libraries(${target} PRIVATE
+    ${test_lib}
+    GTest::gtest_main
+  )
+endfunction()
