5628fce44dfad3a1e52f2db9262d43d309bb2607
hwabis <kennysun23@gmail.com>
2025-03-21 17:26:17 -0400

    actually eradicate all msvc gtest warnings this time

--- cmake/AddTestExecutable.cmake
+++ cmake/AddTestExecutable.cmake
@@ -5,12 +5,17 @@ function(add_test_executable target test_lib)
 
   set_target_defaults(${target})
 
-  target_include_directories(${target} SYSTEM PRIVATE
-    "${googletest_SOURCE_DIR}/include"
-  )
-
   target_link_libraries(${target} PRIVATE
     ${test_lib}
     GTest::gtest_main
   )
+
+  # todo: figure out a better way to suppress all gtest warnings
+  if (MSVC)
+    target_compile_options(${target} PRIVATE
+        /wd26439 # who knows
+        /wd26495 # https://github.com/Azure/azure-sdk-for-cpp/issues/106
+        /wd6326 # https://github.com/google/googletest/issues/471
+    )
+  endif()
 endfunction()
